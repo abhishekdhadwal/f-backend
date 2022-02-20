7d49e09c912df6f015dfe0ee25b358498cee3001
Abhishek Dhadwal <41069780+abhishekdhadwal@users.noreply.github.com>
2022-02-20 17:53:04 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -6,30 +6,35 @@ const { User } = require('../models/User');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+// use like this
+router.post("/login", admin_controller.login)
+
+router.post('/login', async (req, res) => {
     // First Validate The HTTP Request
-    const { error } = validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
+//     es part ko alag file m likho
+//     const { error } = validate(req.body);
+//     if (error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
 
-    //  Now find the user by their email address
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        return res.status(400).send('Incorrect email or password.');
-    }
+//     //  Now find the user by their email address
+//     let user = await User.findOne({ email: req.body.email });
+//     if (!user) {
+//         return res.status(400).send('Incorrect email or password.');
+//     }
 
-    // Then validate the Credentials in MongoDB match
-    // those provided in the request
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) {
-        return res.status(400).send('Incorrect email or password.');
-    }
-    const token = jwt.sign({ _id: user._id }, 'PrivateKey');
-    res.send(token);
+//     // Then validate the Credentials in MongoDB match
+//     // those provided in the request
+//     const validPassword = await bcrypt.compare(req.body.password, user.password);
+//     if (!validPassword) {
+//         return res.status(400).send('Incorrect email or password.');
+//     }
+//     const token = jwt.sign({ _id: user._id }, 'PrivateKey');
+//     res.send(token);
 });
 
-function validate(req) {
+const validate = (req) => {
+//     esko bi alag file m
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required()
